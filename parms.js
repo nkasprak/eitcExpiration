2c9e4cb8095b1063847bd9d785c995cc273c643c
nkasprak <nkasprak@gmail.com>
2015-09-21 15:39:37 -0400

    Updated inflation adjustments
    Thanks Bryann

--- parms.js
+++ parms.js
@@ -21,95 +21,96 @@ var calculator = {};
 		por:	phaseout rate
 	*/
 	
-	p.eitc = {
-		0:	{
-			eiba:	6990,
-			bp:		8750,
-			mpr:	{
-				expiration:	3670,
-				arra:		5860
+		p.eitc = {
+		0: {
+			eiba: 6940,
+			bp: 8690,
+			mpr: {
+				expiration: 3640,
+				arra: 5820
 			},
-			pir:	{
-				expiration:	0.0765,
-				arra:		0.0765
+			pir: {
+				expiration: 0.0765,
+				arra: 0.0765
 			},
-			por:	0.0765
+			por: 0.0765
 		},
 		1: {
-			eiba:	10490,
-			bp:		19240,
-			mpr:	{
-				expiration:	3670,
-				arra:		5860
+			eiba: 10410,
+			bp: 19100,
+			mpr: {
+				expiration: 3640,
+				arra: 5820
 			},
-			pir:	{
-				expiration:	0.34,
-				arra:		0.34
+			pir: {
+				expiration: 0.34,
+				arra: 0.34
 			},
-			por:	0.1598
+			por: 0.1598
 		},
-		2:	{
-			eiba:	14730,
-			bp:		19240,
-			mpr:	{
-				expiration:	3670,
-				arra:		5860
+		2: {
+			eiba: 14620,
+			bp: 19100,
+			mpr: {
+				expiration: 3640,
+				arra: 5820
 			},
-			pir:	{
-				expiration:	0.4,
-				arra:		0.4
+			pir: {
+				expiration: 0.4,
+				arra: 0.4
 			},
-			por:	0.2106
+			por: 0.2106
 		},
-		3:	{
-			eiba:	14730,
-			bp:		19240,
-			mpr:	{
-				expiration:	3670,
-				arra:		5860
+		3: {
+			eiba: 14620,
+			bp: 19100,
+			mpr: {
+				expiration: 3640,
+				arra: 5820
 			},
-			pir:	{
-				expiration:	0.4,
-				arra:		0.45
+			pir: {
+				expiration: 0.4,
+				arra: 0.45
 			},
-			por:	0.2106
+			por: 0.2106
 		}
 	};
 	
 	//storage for child tax credit parameters
 	/*Keys:
-		pt:		phaseout threshold
-		ps:		phaseout step
-		pi:		phaseout interval
-		a:		per child amount
-		r:		rate of refundability
-		eif:	earned income floor
+					pt:                          phaseout threshold
+					ps:                          phaseout step
+					pi:                           phaseout interval
+					a:                            per child amount
+					r:                             rate of refundability
+					eif:         earned income floor
 	*/
 	p.ctc = {
-		pt:	{
-			single:	75000,
-			hoh:	75000,
-			mfj:	110000,
-			mfs:	55000	
+		pt: {
+			single: 75000,
+			hoh: 75000,
+			mfj: 110000,
+			mfs: 55000
 		},
-		ps:		50,
-		pi:		1000,
-		a:		1000,
-		r:		.15,
-		eif:	{
-			expiration:	14700,
-			arra:		3000	
+		ps: 50,
+		pi: 1000,
+		a: 1000,
+		r: 0.15,
+		eif: {
+			expiration: 14600,
+			arra: 3000
 		}
 	};
 	
-	p.sd =  {
-		single:	6700,
-		hoh:	9800,
-		mfj:	13400,
-		mfs:	6700	
-	}
+	p.sd = {
+		single: 6650,
+		hoh: 9750,
+		mfj: 13300,
+		mfs: 6650
+	};
 	
 	p.ex = 4250;
+
 	
 	//list filing statuses in order according to values in selector here for later reference
 	p.fsTranslator = ["single","mfj","hoh","mfs"]; 
